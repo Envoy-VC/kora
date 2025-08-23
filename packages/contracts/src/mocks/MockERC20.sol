fd12459f7171c740a6d1595175c745608db190d8
Vedant Chainani <vedantchainani1084@gmail.com>
2025-08-23 06:41:42 +0530

    chore: update mock token to 6 decimals

--- packages/contracts/src/mocks/MockERC20.sol
+++ packages/contracts/src/mocks/MockERC20.sol
@@ -16,4 +16,8 @@ contract MockERC20 is ERC20, Ownable, ERC20Permit {
     function mint(address to, uint256 amount) public {
         _mint(to, amount);
     }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 6;
+    }
 }
