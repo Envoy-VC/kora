7bc21351b7021a15d98ef8c84a9ef6d4e12c2445
Vedant Chainani <vedantchainani1084@gmail.com>
2025-08-25 04:41:02 +0530

    chore: re-add crons

--- apps/web/src/convex/crons.ts
+++ apps/web/src/convex/crons.ts
@@ -1,14 +1,14 @@
 import { cronJobs } from "convex/server";
 
-// import { api } from "./_generated/api";
+import { api } from "./_generated/api";
 
 const crons = cronJobs();
 
-// crons.hourly(
-//   "Batch Execute Transactions",
-//   { minuteUTC: 30 },
-//   api.functions.actions.executeBatch,
-// );
+crons.hourly(
+  "Batch Execute Transactions",
+  { minuteUTC: 0 },
+  api.functions.actions.executeBatch,
+);
 
 // biome-ignore lint/style/noDefaultExport: safe
 export default crons;
